e9b282f247e938bf0b16c0225b6d066c58de4ae4
Ataibek Isakov <atayisakov558@gmail.com>
2022-01-14 14:07:38 +0100

    camera script, player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 50f;
+    public float turnSpeed = 50f;
+
+    private Rigidbody playerRb;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        playerRb.AddForce(Vector3.right * horizontalInput * speed);
+
     }
 }
