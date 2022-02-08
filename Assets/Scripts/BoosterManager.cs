06b4fcebcf52662312bdd6bcdfd8bd669c024ad9
Bakyt Eshaliev <39187339+BakytEshaliev@users.noreply.github.com>
2022-02-08 09:54:12 +0100

    fix bugs

---
+++ Assets/Scripts/BoosterManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BoosterManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] ghosts;
+    public static BoosterManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public async Task StartBoosterModeAsync()
+    {
+        for(int i = 0; i < ghosts.Length; i++)
+        {
+            GhostController controller = ghosts[i].GetComponent<GhostController>();
+            if (controller.IsActivated()) {
+                controller.StartScaredMode();
+            }
+        }
+        await Task.Delay(7 * 1000);
+        EndBoosterMode();
+    }
+
+    public void EndBoosterMode()
+    {
+        for (int i = 0; i < ghosts.Length; i++)
+        { 
+            GhostController controller = ghosts[i].GetComponent<GhostController>();
+            if (controller.IsScared())
+            {
+                controller.StartPatrolMode();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
